998314454c67b0d3bb082011bbd864ebfdcd12e7
0sunset0 <chi6465618@naver.com>
2022-02-26 17:22:36 +0900

    commit

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,48 @@
+package hello.core.lifecycle;
+
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient{
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작 시 호출
+    public void connect(){
+        System.out.print(" connect: "+ url);
+    }
+
+    public void call(String message){
+        System.out.println(" call : " + url +", message : " + message);
+    }
+
+    //서비스 종료 시 호출
+    public void disconnect(){
+        System.out.print(" close : "+ url);
+    }
+
+    @PostConstruct
+    //의존관계 주입이 끝나면 호출
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    //빈 종료되기 전에 호출 - ac.close() 전
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
